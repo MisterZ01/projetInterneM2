51b49cdd276d23b952671d266c11f8f9cafa9cd5
zoumana.dit.zoum2x <zoumana.dit.zoum2x@gmail.com>
2023-01-25 17:09:20 +0000

    La liste,creer des contrat ok, pareil pour client

--- application/urls.py
+++ application/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('Client/create', views.Client_create, name="Client_create"),
+    path('Client/list', views.Client_list, name="Client_list"),
+    path('Contrat/create', views.Contrat_create, name="Contrat_create"),
+    path('Contrat/list', views.Contrat_list, name="Contrat_list"),
 ]
--- application/views.py
+++ application/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def Client_create(request):
+    return render(request, 'pages/Clients/creer.html')
+
+def Client_list(request):
+    return render(request, 'pages/Clients/liste.html')
+
+def Contrat_create(request):
+    return render(request, 'pages/Contrats/creer.html')
+
+def Contrat_list(request):
+    return render(request, 'pages/Contrats/liste.html')
