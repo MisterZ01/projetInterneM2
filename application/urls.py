78e18895a01de9493c0ed5b01e7ffa76d2ecbb22
zoumana.dit.zoum2x <zoumana.dit.zoum2x@gmail.com>
2023-01-27 17:22:56 +0000

    ajout des routes des clients, panneaux et contrats

--- application/urls.py
+++ application/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('Client/list', views.Client_list, name="Client_list"),
     path('Contrat/create', views.Contrat_create, name="Contrat_create"),
     path('Contrat/list', views.Contrat_list, name="Contrat_list"),
+    path('Panneau/create', views.Panneau_create, name="Panneau_create"),
+    path('Panneau/list', views.Panneau_list, name="Panneau_list"),
 ]
--- application/views.py
+++ application/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 
+
+
+# accueil
 def index(request):
     return render(request, 'index.html')
-
+# client
 def Client_create(request):
     return render(request, 'pages/Clients/creer.html')
 
 def Client_list(request):
     return render(request, 'pages/Clients/liste.html')
 
+# panneaux
+def Panneau_create(request):
+    return render(request, 'pages/Panneaux/creer.html')
+
+def Panneau_list(request):
+    return render(request, 'pages/Panneaux/liste.html')
+
+# contrat
 def Contrat_create(request):
     return render(request, 'pages/Contrats/creer.html')
 
