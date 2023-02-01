90b857e5dc42b3238f7a42ea20adba7eb662299b
zoumana.dit.zoum2x <zoumana.dit.zoum2x@gmail.com>
2023-02-01 16:51:45 +0000

    ajoute d'une partie pour le test de l'exerciste api odc

--- application/urls.py
+++ application/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('Contrat/list', views.Contrat_list, name="Contrat_list"),
     path('Panneau/create', views.Panneau_create, name="Panneau_create"),
     path('Panneau/list', views.Panneau_list, name="Panneau_list"),
+    path('usageapi', views.usageapi, name="usageapi"),
+    path('detail', views.detail, name="detail"),
+    path('edit/<int:id>', views.edit, name="edit"),
+    path('delete/<int:id>', views.delete, name="delete"),
 ]
