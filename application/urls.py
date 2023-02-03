0c9b51531273a77ee4ed91eee62cf1808ff25ae9
zoumana.dit.zoum2x <zoumana.dit.zoum2x@gmail.com>
2023-02-03 18:07:21 +0000

    liste clients ok avec api laravel, detail ok, edit nok

--- application/urls.py
+++ application/urls.py
@@ -3,14 +3,18 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="index"),
+    # debut client 
     path('Client/create', views.Client_create, name="Client_create"),
     path('Client/list', views.Client_list, name="Client_list"),
+    path('detail/<int:id>', views.Client_detail, name="Client_detail"),
+    path('edit/<int:id>', views.Client_edit, name="Client_edit"),
+    path('delete/<int:id>', views.Client_delete, name="Client_delete"), 
+
+    # debut contrat 
     path('Contrat/create', views.Contrat_create, name="Contrat_create"),
     path('Contrat/list', views.Contrat_list, name="Contrat_list"),
+    # debut panneau 
     path('Panneau/create', views.Panneau_create, name="Panneau_create"),
     path('Panneau/list', views.Panneau_list, name="Panneau_list"),
     path('usageapi', views.usageapi, name="usageapi"),
-    path('detail', views.detail, name="detail"),
-    path('edit/<int:id>', views.edit, name="edit"),
-    path('delete/<int:id>', views.delete, name="delete"),
 ]
--- application/views.py
+++ application/views.py
@@ -11,37 +11,47 @@ def Client_create(request):
     return render(request, 'pages/Clients/creer.html')
 
 def Client_list(request):
-    return render(request, 'pages/Clients/liste.html')
+    reponse = requests.get("http://127.0.0.1:8000/api/clients").json()
+    return render(request, 'pages/Clients/liste.html',{'data':reponse})
+
+def Client_detail(request, id):
+    link = "http://127.0.0.1:8000/api/clients/"+str(id)
+    reponse = requests.get(link).json()
+    print(link)
+    return render(request, 'pages/Clients/detail.html', {'data':reponse})
+
+def Client_edit(request, id):
+    link = "http://127.0.0.1:8000/api/clients/"+str(id)+"/edit"
+    reponse = requests.get(link).json()
+    return render(request, 'pages/Panneaux/usageapi.html', {'data':reponse})
+
+def Client_delete(request, id):
+    r=  requests.post("http://127.0.0.1:8000/api/client/clients/{id}").json()
+    return render(request, "a")
 
 # panneaux
 def Panneau_create(request):
+    
     return render(request, 'pages/Panneaux/creer.html')
 
 def Panneau_list(request):
-    return render(request, 'pages/Panneaux/liste.html')
+    reponse = requests.get("http://127.0.0.1:8000/api/panneaus").json()
+    return render(request, 'pages/Panneaux/liste.html',{'data':reponse})
 
 # contrat
 def Contrat_create(request):
     return render(request, 'pages/Contrats/creer.html')
 
 def Contrat_list(request):
-    return render(request, 'pages/Contrats/liste.html')
+    reponse = requests.get("http://127.0.0.1:8000/api/contrats").json()
+    return render(request, 'pages/Contrats/liste.html',{'data':reponse})
 
 
 # ---------------------------------------------- projet odc test api ----------------------------
 # test de mon api
 def usageapi(request):
-    reponse = requests.get("http://127.0.0.1:8000/api/all").json()
-    return render(request, 'pages/Panneaux/usageapi.html', {'data':reponse})
-# detail
-def detail(request, id):
-    reponse = requests.get("http://127.0.0.1:8000/api/all").json()
-    return render(request, 'pages/Panneaux/usageapi.html', {'data':reponse})
-# edit
-def edit(request, id):
-    reponse = requests.post("http://127.0.0.1:8000/api//update/{id}").json()
-    return render(request, 'pages/Panneaux/usageapi.html', {'data':reponse})
-# delete
-def delete(request, id):
-    r=  requests.post("http://127.0.0.1:8000/api/item/1/delete").json()
-    return render(request, "a")
+    reponse1 = requests.get("http://127.0.0.1:8000/api/clients").json()
+
+    return render(request, 'pages/Panneaux/usageapi.html', {'data':reponse1})
+
+
